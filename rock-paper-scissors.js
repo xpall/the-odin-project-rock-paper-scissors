5b8b03c051f39092b279e3c45266f205cabd5ce7
xpall <127076043+xpall@users.noreply.github.com>
2023-08-11 23:09:08 +0800

    Rearranged code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,69 +2,15 @@ function main() {
   return game();
 }
 
-function getComputerChoice() {
-  const choices = ['Rock', 'Paper', 'Scissor'];
-  let choice = (choices[Math.floor(Math.random() * choices.length)]);
-  return choice.toLocaleLowerCase();
-}
-
-function computerSelection() {
-  return getComputerChoice();
-}
-
-function playerSelection() {
-  let choice = prompt('Rock, Paper, or Scissors?');
-  return choice.toLowerCase(); 
-}
-
-// Writing playRound using nested ifs
-
-// function playRound(playerSelection, computerSelection) {
-//   if (playerSelection == 'rock' && computerSelection == 'paper') {
-//     return 'You lose! Paper beats Rock';
-//   } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-//     return 'You win! Rock beats Scissors';
-//   } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-//     return 'You win! Paper beats Rock';
-//   } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-//     return 'You lose! Scissors beat Paper';
-//   } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-//     return 'You win! Scissors beat Paper';
-//   } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-//     return 'You lose! Rock beats Scissors';
-//   } else {
-//     return 'It\'s a tie'
-//   }
-// }  
-
-// Writing playRound using switch and ternary
-
-function playRound(playerSelection, computerSelection) {
-  switch(playerSelection) {
-    case 'rock':
-      return (computerSelection == 'paper') ? 'You lose! Paper beats rock'
-        : (computerSelection == 'scissors')
-          ? 'You win! Rock beats scissors'
-          : 'It\'s a tie.'
-    case 'paper':
-      return (computerSelection == 'scissors') ? 'You lose! Scissors beat rock'
-        : (computerSelection == 'rock')
-          ? 'You win! Paper beats rock'
-          : 'It\'s a tie.'
-    case 'scissors':
-      return (computerSelection == 'rock') ? 'You lose! Rock beats scissors'
-        : (computerSelection == 'paper')
-          ? 'You win! Scissors beat paper'
-          : 'It\'s a tie.'
-  }
-}
-
 function game() {
   let playerScore = 0;
   let computerScore = 0;
   let ties = 0;
   for (i = 0; i < 5; i++) {
-    gameResult = playRound(playerSelection(), computerSelection());
+    let human = playerSelection();
+    let computer = computerSelection();
+    console.log(`${human} vs ${computer}`)
+    gameResult = playRound(human, computer);
     console.log(gameResult)
     if (gameResult[4] == 'w') {
       playerScore += 1;
@@ -74,7 +20,59 @@ function game() {
       ties += 1;
     }
   }
-  return `Five rounds are over, you got ${playerScore} wins and ${(5 - playerScore)} lose(s). Number of ties are ${ties}.`
+  console.log(`Five rounds are over, you got ${playerScore} wins and ${(computerScore)} lose(s). Number of ties are ${ties}.`)
+}
+
+// Writing playRound using nested ifs
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == 'rock' && computerSelection == 'paper') {
+    return 'You lose! Paper beats Rock';
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return 'You win! Rock beats Scissors';
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return 'You win! Paper beats Rock';
+  } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    return 'You lose! Scissors beat Paper';
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    return 'You win! Scissors beat Paper';
+  } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    return 'You lose! Rock beats Scissors';
+  } else {
+    return 'It\'s a tie'
+  }
+}  
+
+function playerSelection() {
+  let choice = prompt('Rock, Paper, or Scissors?');
+  return choice.toLowerCase(); 
+}
+
+function computerSelection() {
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  let choice = (choices[Math.floor(Math.random() * choices.length)]);
+  return choice.toLocaleLowerCase();
 }
 
 main();
+
+// Writing playRound using switch and ternary
+
+// function playRound(playerSelection, computerSelection) {
+//   switch(playerSelection) {
+//     case 'rock':
+//       return (computerSelection == 'paper') ? 'You lose! Paper beats rock'
+//         : (computerSelection == 'scissors')
+//           ? 'You win! Rock beats scissors'
+//           : 'It\'s a tie.'
+//     case 'paper':
+//       return (computerSelection == 'scissors') ? 'You lose! Scissors beat rock'
+//         : (computerSelection == 'rock')
+//           ? 'You win! Paper beats rock'
+//           : 'It\'s a tie.'
+//     case 'scissors':
+//       return (computerSelection == 'rock') ? 'You lose! Rock beats scissors'
+//         : (computerSelection == 'paper')
+//           ? 'You win! Scissors beat paper'
+//           : 'It\'s a tie.'
+//   }
+// }
